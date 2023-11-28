63f91a3071571931751b3f82b9a418e8c712c3ac
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:25:19 +0100

    correcting buildapp code reset to the last commit

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,21 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
-                echo 'Building the App...'
-
-                // Create a virtual environment
-                sh 'python -m venv venv'
-
-                // Activate the virtual environment
-                sh 'source venv/bin/activate'
-
-                // Install required packages within the virtual environment
-                sh 'pip install jupyter nbconvert'
-                sh 'pip install -r requirements.txt'
-
-                // Deactivate the virtual environment
-                sh 'deactivate'
-
-                // Run your other commands (if any) outside the virtual environment
-                // sh 'python -u eau2c2_dashcore.py'
+    echo "Building the App..."
+    sh "pip install jupyter nbconvert"
+    sh "pip install -r requirements.txt"
     // sh "python -u eau2c2_dashcore.py"
 }
