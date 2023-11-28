635d9569addc1e937cc9b43484e8420dcfdc8006
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:56:56 +0100

    path1

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,11 +1,15 @@
 #!/usr/bin/env groovy
 def call() {
-    echo "Building the App..."
-     // Specify the full path to the requirements.txt file in the other project
-    def requirementsFilePath = '/app/requirements.txt'
+   dir('app') {
+        echo "Current directory: ${PWD}"
+        echo "Contents of the directory:"
+        sh "ls -la"
 
-    sh "pip install jupyter nbconvert"
-    sh "pip install -r ${requirementsFilePath}"
-    echo "${requirementsFilePath}"
-    // sh "python -u eau2c2_dashcore.py"
+        // Specify the path relative to Jenkins workspace
+        def requirementsFilePath = "${PWD}/requirements.txt"
+
+        sh "pip install jupyter nbconvert"
+        sh "pip install -r ${requirementsFilePath}"
+        // sh "python -u eau2c2_dashcore.py"
+    }
 }
