13efb45bd09bcaa27962ce8e7e173c2caf7110a4
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:35:44 +0100

    adding the path to requirements.txt

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 def call() {
     echo "Building the App..."
+     // Specify the full path to the requirements.txt file in the other project
+    def requirementsFilePath = '/home/rym/eau2c2/eau2c2/app/requirements.txt'
+
     sh "pip install jupyter nbconvert"
-    sh "pip install -r requirements.txt"
+    sh "pip install -r ${requirementsFilePath}"
     // sh "python -u eau2c2_dashcore.py"
 }
