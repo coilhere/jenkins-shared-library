0e9f3bc7dee6b86143944912b1fa782c8465339e
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:19:13 +0100

    correcting buildapp code

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,21 @@
 #!/usr/bin/env groovy
 def call() {
-    echo "Building the App..."
-    sh "pip install jupyter nbconvert"
-    sh "pip install -r requirements.txt"
+                echo 'Building the App...'
+
+                // Create a virtual environment
+                sh 'python -m venv venv'
+
+                // Activate the virtual environment
+                sh 'source venv/bin/activate'
+
+                // Install required packages within the virtual environment
+                sh 'pip install jupyter nbconvert'
+                sh 'pip install -r requirements.txt'
+
+                // Deactivate the virtual environment
+                sh 'deactivate'
+
+                // Run your other commands (if any) outside the virtual environment
+                // sh 'python -u eau2c2_dashcore.py'
     // sh "python -u eau2c2_dashcore.py"
 }
