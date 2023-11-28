cfa472d435e98be636371e0fc1902498d548195e
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:00:50 +0100

    correcting buildapp function

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
-def call(){
-    echo"Building the App..."
-    sh "run pip install jupyter nbconvert"
-    sh"run pip install -r requirements.txt"
-    sh" python -u eau2c2_dashcore.py"
-
+def call() {
+    echo "Building the App..."
+    sh "pip install jupyter nbconvert"
+    sh "pip install -r requirements.txt"
+    // sh "python -u eau2c2_dashcore.py"
 }
