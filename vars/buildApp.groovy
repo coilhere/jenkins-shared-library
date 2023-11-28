7f19677dd52cdf01642026f0a318466b13bbf8d3
Rym chaouch <chaouchrim22@gmail.com>
2023-11-28 14:50:56 +0100

    verifying workspace

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,11 +1,12 @@
 #!/usr/bin/env groovy
 def call() {
-    echo "Building the App..."
-     // Specify the full path to the requirements.txt file in the other project
-    def requirementsFilePath = '/home/rym/eau2c2/eau2c2/app/requirements.txt'
+   echo "Building the App..."
+    echo "Current directory: ${PWD}"
+    echo "Contents of the directory:"
+    sh "ls -la"
+    echo "Requirements file path: ${requirementsFilePath}"
 
     sh "pip install jupyter nbconvert"
     sh "pip install -r ${requirementsFilePath}"
-    echo "${requirementsFilePath}"
     // sh "python -u eau2c2_dashcore.py"
 }
