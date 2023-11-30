bca2d01cc2a46d303c234bc8efedf7d59ffd6114
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 16:54:01 +0100

    correcting  instead of '' in build -t

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,7 +5,7 @@ def call(String imageName){
         echo "Contents of the directory:"
         sh "ls -la"
         echo "Current directory: ${PWD}"
-        sh 'docker build -t $imageName -f ./app/Dockerfile.dev .'
+        sh "docker build -t $imageName -f ./app/Dockerfile.dev ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         // sh "docker tag eau2c2:1.0 44.204.7.163:8083/eau2c2:1.0"  // Fixed tag command
 
