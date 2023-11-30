db640e21de7ab9163a919f542f47a70004683326
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:09:49 +0100

    correcting the port

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call(){
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t eau2c2:1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh" tag eau2c2:1.0 52.207.215.103:8080/eau2c2:1.0"
-        sh "docker push 52.207.215.103:8080/eau2c2:1.0 "
+        sh" tag eau2c2:1.0 http://3.82.130.20:8083//eau2c2:1.0"
+        sh "docker push http://3.82.130.20:8083/eau2c2:1.0 "
 }
 }
