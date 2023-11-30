f4e79bf6ec60f9f981a6ed3605fb84da4cab814c
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:27:54 +0100

    replace . by Dockerfile.dev

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 def call(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t eau2c2:1.0 .'
+        sh 'docker build -t eau2c2:1.0 Dockerfile.dev'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh" tag eau2c2:1.0 http://3.82.130.20:8083//eau2c2:1.0"
         sh "docker push http://3.82.130.20:8083/eau2c2:1.0 "
