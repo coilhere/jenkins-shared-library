1c3539e13c2532a206077511d13960d909a1db76
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:50:53 +0100

    pwd

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,6 +4,7 @@ def call(){
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         echo "Contents of the directory:"
         sh "ls -la"
+        echo "Current directory: ${PWD}"
         sh 'docker build -t eau2c2:1.0 Dockerfile.dev'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh" tag eau2c2:1.0 3.82.130.20:8083//eau2c2:1.0"
