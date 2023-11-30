ccaed5a6c0c06a0136d8fd5725aef7343e98b288
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:31:02 +0100

    verifyng the path for dockerfile

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,6 +2,8 @@
 def call(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        echo "Contents of the directory:"
+        sh "ls -la"
         sh 'docker build -t eau2c2:1.0 Dockerfile.dev'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh" tag eau2c2:1.0 http://3.82.130.20:8083//eau2c2:1.0"
