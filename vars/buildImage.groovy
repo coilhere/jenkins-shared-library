7de5891004ed861f46432231184f8399f988280a
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 14:30:29 +0100

    push to dockerhub

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,14 +1,15 @@
 #!/usr/bin/env groovy
 def call(){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         echo "Contents of the directory:"
         sh "ls -la"
         echo "Current directory: ${PWD}"
         sh 'docker build -t eau2c2:1.0 -f ./app/Dockerfile.dev .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 44.204.7.163:8083"
-        sh "docker tag eau2c2:1.0 44.204.7.163:8083/eau2c2:1.0"  // Fixed tag command
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        // sh "docker tag eau2c2:1.0 44.204.7.163:8083/eau2c2:1.0"  // Fixed tag command
 
-        sh "docker push 44.204.7.163:8083/eau2c2:1.0 "
+        // sh "docker push 44.204.7.163:8083/eau2c2:1.0 "
+         sh "docker push rymm99/eau2c2:1.0"
 }
 }
