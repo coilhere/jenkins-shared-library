f0809a621b23911bb8e392fa5f27f02803d821ce
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 13:30:52 +0100

    .

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -7,7 +7,7 @@ def call(){
         echo "Current directory: ${PWD}"
         sh 'docker build -t eau2c2:1.0 -f ./app/Dockerfile.dev .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh" tag eau2c2:1.0 3.82.130.20:8083//eau2c2:1.0"
+        sh" tag eau2c2:1.0 3.82.130.20:8083/eau2c2:1.0"
         sh "docker push 3.82.130.20:8083/eau2c2:1.0 "
 }
 }
