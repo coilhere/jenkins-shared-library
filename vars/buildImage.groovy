1cf627c30ad8a8699d4f0fe79dea166c4295be3e
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 14:39:19 +0100

    rectifaying buildimage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -10,6 +10,10 @@ def call(){
         // sh "docker tag eau2c2:1.0 44.204.7.163:8083/eau2c2:1.0"  // Fixed tag command
 
         // sh "docker push 44.204.7.163:8083/eau2c2:1.0 "
-         sh "docker push rymm99/eau2c2:1.0"
-}
+        // Tag the Docker image with the registry information
+        sh 'docker tag eau2c2:1.0 rymm99/eau2c2:1.0'
+
+        // Push the Docker image to the registry
+        sh 'docker push rymm99/eau2c2:1.0'
+        }
 }
