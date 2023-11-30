894d85b182c140cd930d6ee1c739a51047326255
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:48:39 +0100

    giving -f to path for dockerfile corrected 3

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,17 +1,12 @@
 #!/usr/bin/env groovy
-
-def call() {
+def call(){
     echo "building the docker image..."
-    
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         echo "Contents of the directory:"
-        dir("/home/rym/eau2c2/eau2c2/app") {
-            sh 'ls -la'
-            sh 'docker build -t eau2c2:1.0 -f Dockerfile.dev .'
-        }
-        
+        sh "ls -la"
+        sh 'docker build -t eau2c2:1.0 Dockerfile.dev'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker tag eau2c2:1.0 http://3.82.130.20:8083/eau2c2:1.0"
-        sh "docker push http://3.82.130.20:8083/eau2c2:1.0"
-    }
+        sh" tag eau2c2:1.0 3.82.130.20:8083//eau2c2:1.0"
+        sh "docker push 3.82.130.20:8083/eau2c2:1.0 "
+}
 }
