9c3d0ac972c690538581a1431247c21bc8622143
Rym chaouch <chaouchrim22@gmail.com>
2023-11-30 12:54:01 +0100

    dockerfile adding .

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,7 +5,7 @@ def call(){
         echo "Contents of the directory:"
         sh "ls -la"
         echo "Current directory: ${PWD}"
-        sh 'docker build -t eau2c2:1.0 -f ./app/Dockerfile.dev'
+        sh 'docker build -t eau2c2:1.0 -f ./app/Dockerfile.dev .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh" tag eau2c2:1.0 3.82.130.20:8083//eau2c2:1.0"
         sh "docker push 3.82.130.20:8083/eau2c2:1.0 "
